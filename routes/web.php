951887eb042b7ad932214cab2604ece93cd523e1
Riat Abduramani <riat.abduramani@gmail.com>
2018-11-08 09:31:14 +0100

    Working on routes

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,6 @@ $get_all_users_handler = $user->users();
 
 $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
     $r->addRoute('GET', '/users', 'Customproject\\Application\\Controllers\\User/users');
-    // {id} must be a number (\d+)
-    $r->addRoute('GET', '/user/{id:\d+}', 'get_user_handler');
-    // The /{title} suffix is optional
-    $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
 });
 
 
